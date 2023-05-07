7615553e06b4e7b5ba691bd1c424c92896c76358
obyx <cole@obyx.cc>
2023-05-07 11:55:16 -0400

    font changes

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:style=Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:style=Regular:size=14";
 static int borderpx = 20;
 
 /*
